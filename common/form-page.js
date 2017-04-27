247dbfe390a123837b56cf47d779e8eb2d15adbf
scallsen <simon.callsen@gmail.com>
2017-04-27 08:43:52 -0400

    solved all form errors

---
+++ common/form-page.js
@@ -0,0 +1,9 @@
+$('.section-1-btn').on('click', function(e) {
+  e.preventDefault();
+  $('.section-1').toggleClass('hide');
+});
+
+$('.section-1-btn').on('click', function(e) {
+  e.preventDefault();
+  $('.section-2').toggleClass('hide');
+});
